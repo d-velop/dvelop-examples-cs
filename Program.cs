097bbbffd602077a42599571f39130ef827d0724
Dominikus Bartusch <dbar@d-velop.de>
2019-10-08 11:54:05 +0200

    example adapted to d.velop cloud instance

--- Program.cs
+++ Program.cs
@@ -17,57 +17,74 @@ namespace DMSAPISamples
 
         static void Main(string[] args) 
         {
-            //Important: Tls 1.0 is no longer supported! Please use the next line of code for compatibilty settings or select the most recent .NET Framework for this project!
-            ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+            //These samples refer to the RESTful interfaces of the d.ecs apps DMSApp and IdentityProviderApp which 
+            //are contained in d.3one. Cloud and on-premises instances can be used both.
 
-            //These samples refer to a d.3one application (d.velop cloud or on-premises). 
-            //You can register for a 30day free d.velop cloud here: https://order.d-velop.cloud/register
+            //Follow these steps before running the code:
 
-            //Alternatively you can use the open cloud development system https://tp-dev.d-velop.cloud. To get an user account please contact the d.velop Technology Partner Management.
-            //Please note, that any uploaded data is visible to other development partners
+            //ONE: you need the base uri of your d.3one instance. To get an instance in the d.velop cloud and to 
+            //choose a base uri, click here: https://store.d-velop.de/9/d.velop-documents
+//            var baseURI = @"https://xxxxxxx.d-velop.cloud";
+            var baseURI = @"https://test-dbar4.d-velop.cloud";
 
-            //To run with other d.3one applications or d-velop clouds, please adjust baseURI
-            //apiKey or username, password and repoId as well as the metadata provided in the files upload.json and search.txt and the 
-            //directory path to local sample files. Then copy the files from the project folder "sample files" to this directory. 
-            //The used source mapping ist shown in file source.json.
+            //TWO: get an api key for your user in your d.3one instance: In d.one, click on tile "IdentityProvider", 
+            //then on the "fingerprint icon" on the right.
+//            var apiKey = @"xxxxxxxxxxxxxxxxxx";
+            var apiKey = @"BYNhvn8F7Dvs1AjDsfzc6JFiWAd8o5eyMejeO/NIASsRi5CS8ScE78Wwxy1dCRqCCWKrtWHXn8pcViu7V6odcS4g1yZgVnwXQWOFEyp10wc=&_z_A0V5ayCQwWUJbRag_CqV9rDY6Ffznns-BcJUcClCcdagCNXUf6bl7JqwN-vEtPRK2WdcaL5e3Rhz370kikslYf6-Hkm-n";
 
-            //local path to sample files
-            var filePath = @"C:\temp";
+            //THREE: get a repository id from your d.3one instance: In d.3one, click on tile "Search", then select 
+            //the repository from the combobox on the top, copy the repository id form the browser URL
+//            var repoId = @"xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx";
+            var repoId = @"47f01950-9560-47de-9734-c0afae5fa033";
 
-            //base uri of your target d.3one application. Use the following Uri for the open development system: "https://tp-dev.d-velop.cloud"
-            var baseURI = @"https://tp-dev.d-velop.cloud";
-            //api key of a valid d.3one account. If not available use basic authentication with username and password as described below 
-//            var apiKey = <apiKey>;
-            //id of the repository. In instance tp-dev.d-velop.cloud use: "44330b47-508c-58bd-9109-cedd2c31e418"
-            var repoId = @"44330b47-508c-58bd-9109-cedd2c31e418";
-            //metadata for document upload. In instance tp-dev.d-velop.cloud use the default. Otherwise create your own file according to file "upload.json"
-            var uploadMappingFile = filePath + @"\upload.json";
-            //metadata for document search. In instance tp-dev.d-velop.cloud use the default. Otherwise create your own file according to file "search.txt"
-            var searchStringFile = filePath + @"\search.txt";
+            //FOUR: grant user rights: In d.3one click on tile "Usermanagement", select your user and assign the user group 
+            //"Kundenakte Vollzugriff". Then log off and sign in.
+
+            //FIVE (optional): Provide your own source or proceed with step 6.
+            //This sample is based on the default source "Basisdokumentarten" contained in d.velop documents.
+
+            //SIX: create a mapping between source and content of your d.velop cloud: In d.3one, click on tile "Mappings" and then on 
+            //"create new". Select the source (default: "Basisdokumentarten") and map categories and properties.
+            //Category: "Schriftverkehr Kunde" -> "correspondance customer"
+            //Properties: "Betreff" -> "Betreff", "Kunden-Nummer" -> "Customer No", "Kunden-Name1" -> "Kunden-Name1",
+            //"Beleg-Datum" -> "Beleg-Datum", "Dateiname" -> "File name"
+
+            //SEVEN: copy sample files from project folder "sample files" to local directory and set filePath to this directory
+            var filePath = @"c:\temp";
+
+            //EIGHT: run this code.
 
 
+            //metadata for document upload. In instance tp-dev.d-velop.cloud use the default. Otherwise create your 
+            //own file according to file "upload.json"
+            var uploadMappingFile = Path.Combine(filePath, "upload.json");
+            //metadata for document search. In instance tp-dev.d-velop.cloud use the default. Otherwise create your 
+            //own file according to file "search.txt"
+            var searchStringFile = Path.Combine(filePath, "search.txt");
             var uploadFile = Path.Combine(filePath, "upload.pdf");
             var downloadFilePath = Path.Combine(filePath, "download");
 
-            //using apiKey authentication is recommended! In this case call replace placeholders for username with empty string "" and password with the apiKey
-            //to authenticate with user credentials and base authentication: replace placeholders for username and password 
-            var apiKey = AuthenticateBasic(baseURI, "<empty or username>", "<apiKey or password>");
+            //Important: Tls 1.0 is no longer supported! Please use the next line of code for compatibilty settings or 
+            //select the most recent .NET Framework for this project!
+            ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
 
-            //for all further api calls use the returned key as Bearer-Token!
+            //using apiKey authentication is recommended! 
+            //for all further api calls use the returned sessionId as Bearer-Token!
+            var sessionId = AuthenticateBasic(baseURI, "", apiKey);
 
-            if (null != apiKey)
+            if (null != sessionId)
             {
-                //upload chunked document 
-                var documentLink = UploadDocument(baseURI, apiKey, repoId, uploadFile, uploadMappingFile).Result;
-                //show document in d.3one
+                //upload file as a whole or chunked
+                var documentLink = UploadFile(baseURI, sessionId, repoId, uploadFile, uploadMappingFile).Result;
+//                var documentLink = UploadFileChunked(baseURI, sessionId, repoId, uploadFile, uploadMappingFile).Result;
+                //show uploaded document in d.3one
                 System.Diagnostics.Process.Start(baseURI + documentLink);
-                //get download url and document properties (returned properties are defined in the source mapping)
-                var documentInfo = GetDocumentInfo(baseURI, apiKey, repoId, documentLink).Result;
+                //get download url and document properties (returned properties are defined in the source mapping, see above)
+                var documentInfo = GetDocumentInfo(baseURI, sessionId, repoId, documentLink).Result;
                 //download document
-                bool downloaded = DownloadDocument(baseURI, apiKey, repoId, documentInfo, downloadFilePath).Result;
-
+                bool downloaded = DownloadDocument(baseURI, sessionId, repoId, documentInfo, downloadFilePath).Result;
                 //search documents
-                var result = SearchDocument(baseURI, apiKey, repoId, searchStringFile).Result;
+                var result = SearchDocument(baseURI, sessionId, repoId, searchStringFile).Result;
             }
             Console.ReadKey();
         }
@@ -101,6 +118,7 @@ namespace DMSAPISamples
                         foreach (Cookie cookie in responseCookies)
                             if ("AuthSessionId".Equals(cookie.Name))
                             {
+                                //Important: URL decode the returned value!
                                 response = System.Web.HttpUtility.UrlDecode(cookie.Value);
                                 break;
                             }
@@ -191,7 +209,7 @@ namespace DMSAPISamples
             return null;
         }
 
-        private async static Task<bool> DownloadDocument(string baseURI, string apiKey, string repoId, string jsonDocumentInfo, string downloadFilePath)
+        private async static Task<bool> DownloadDocument(string baseURI, string sessionId, string repoId, string jsonDocumentInfo, string downloadFilePath)
         {
             dynamic documentInfo = JsonConvert.DeserializeObject<object>(jsonDocumentInfo);
             var link_relation =  documentInfo._links.mainblobcontent.href;
@@ -200,7 +218,7 @@ namespace DMSAPISamples
             var fileName = "";
             foreach (var prop in documentInfo.sourceProperties)
             {
-                if (prop.key == "filename")
+                if (prop.key == "dateiname")
                 {
                     fileName = prop.value;
                     break;
@@ -214,7 +232,7 @@ namespace DMSAPISamples
                 //set Origin header to avoid conflicts with same origin policy
                 client.BaseAddress = new Uri(baseURI);
                 client.DefaultRequestHeaders.Add("Origin", baseURI);
-                client.DefaultRequestHeaders.Add("Authorization", "Bearer " + apiKey);
+                client.DefaultRequestHeaders.Add("Authorization", "Bearer " + sessionId);
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(MEDIA_TYPE_HAL_JSON));
 
                 Console.WriteLine("start document download");
@@ -236,8 +254,27 @@ namespace DMSAPISamples
             return false;
         }
 
-        //upload documents in one or more chunks
-        private async static Task<string> UploadDocument(string baseURI, string apiKey, string repoId, string filePath, string uploadMappingFile)
+        //upload file in a whole
+        private async static Task<string> UploadFile(string baseURI, string sessionId, string repoId, string filePath, string uploadMappingFile)
+        {
+            var contentLocationUri = "/dms/r/" + repoId + "/blob/chunk";
+
+            //first: upload file and get an URI (contentLocationUri) as a reference to this file
+            Console.WriteLine("start file upload");
+            contentLocationUri = await UploadFileChunk(baseURI, sessionId, contentLocationUri, filePath);
+
+            //second: upload metadata with reference URI (contentLocationUri).
+            var documentLink = await FinishFileUpload(baseURI, sessionId, repoId, contentLocationUri, filePath, uploadMappingFile);
+            if (null != documentLink)
+            {
+                Console.WriteLine("finished file upload");
+                return documentLink;
+            }
+            return string.Empty;
+        }
+
+        //upload file in chunks
+        private async static Task<string> UploadFileChunked(string baseURI, string sessionId, string repoId, string filePath, string uploadMappingFile)
         {
             var path = Path.GetDirectoryName(filePath);
             var name = Path.GetFileNameWithoutExtension(filePath);
@@ -246,25 +283,25 @@ namespace DMSAPISamples
             var chunkFilePath = Path.Combine(path, name + index);
 
             //first: upload file and get an URI (contentLocationUri) as a reference to this file
-            Console.WriteLine("start document upload");
+            Console.WriteLine("start chunked file upload");
             while (File.Exists(chunkFilePath))
             {
-                contentLocationUri = await UploadDocumentChunk(baseURI, apiKey, contentLocationUri, chunkFilePath);
+                contentLocationUri = await UploadFileChunk(baseURI, sessionId, contentLocationUri, chunkFilePath);
                 chunkFilePath = Path.Combine(path, name + ++index);
             }
 
             //second: upload metadata with reference URI (contentLocationUri).
-            var documentLink = await FinishChunkedDocumentUpload(baseURI, apiKey, repoId, contentLocationUri, filePath, uploadMappingFile);
+            var documentLink = await FinishFileUpload(baseURI, sessionId, repoId, contentLocationUri, filePath, uploadMappingFile);
             if (null != documentLink)
             {
-                Console.WriteLine("finished document upload");
+                Console.WriteLine("finished chunked file upload");
                 return documentLink;
             }
             return string.Empty;
         }
 
 
-        private async static Task<string> UploadDocumentChunk(string baseURI, string apiKey, string link_relation, string chunkFilePath)
+        private async static Task<string> UploadFileChunk(string baseURI, string apiKey, string link_relation, string chunkFilePath)
         {
             var baseRequest = baseURI + link_relation;
 
@@ -298,7 +335,7 @@ namespace DMSAPISamples
         }
 
 
-        private static async Task<string> FinishChunkedDocumentUpload(string baseURI, string apiKey, string repoId, string contentLocationUri, string filePath, string uploadMappingFile)
+        private static async Task<string> FinishFileUpload(string baseURI, string apiKey, string repoId, string contentLocationUri, string filePath, string uploadMappingFile)
         {
             var link_relation = "/dms/r/" + repoId + "/o2m";
             var baseRequest = baseURI + link_relation;
