035853cea0737fbc33fa16551d6cc1693973025f
Dominikus Bartusch <dbar@d-velop.de>
2019-10-22 11:40:01 +0200

    Funktion URLEncode entfernt

--- Program.cs
+++ Program.cs
@@ -350,13 +350,5 @@ namespace DMSAPISamples
                 }
             }
         }
-
-
-        private static string Base64Encode(string plainText)
-        {
-            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
-            return System.Convert.ToBase64String(plainTextBytes);
-        }
-
     }
 }
